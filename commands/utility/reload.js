810c13f5f91aedabfe82824fc7277aa64e4c3a52
Liam Greer <Tacxam7@gmail.com>
2026-02-17 15:40:13 +1100

    setup code

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -6,6 +6,10 @@ module.exports = {
 		.setDescription('Reloads a command.')
 		.addStringOption((option) => option.setName('command').setDescription('The command to reload.').setRequired(true)),
 	async execute(interaction) {
-		// ...
+		const commandName = interaction.options.getString('command', true).toLowerCase();
+		const command = interaction.client.commands.get(commandName);
+		if (!command) {
+			return interaction.reply(`There is no command with name \`${commandName}\`!`);
+		}
 	},
 };
