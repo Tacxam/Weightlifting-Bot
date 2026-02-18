ec2c44489450149ebde9e5f71d8200d20bab574d
Liam Greer <Tacxam7@gmail.com>
2026-02-18 21:26:36 +1100

    added auto complete for addscore

--- commands/leaderboard/addScore.js
+++ commands/leaderboard/addScore.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const exerciseChoices = require('../../utils/exerciseChoices.js');
 
 // Add score to leaderboard (Delete any previous score from the leaderboard)
 module.exports = {
@@ -6,13 +7,13 @@ module.exports = {
 		.setName('addscore')
 		.setDescription('Add a score to the leaderboard.')
 		.addStringOption((option) => option.setName('weight').setDescription('The weight being submitted.').setRequired(true))
-		.addStringOption((option) => option.setName('exercise').setDescription('The exercise being submitted.').setRequired(true)),
+		.addStringOption((option) => option.setName('exercise').setDescription('The exercise being submitted.').setRequired(true).addChoices(...exerciseChoices)),
 	async execute(interaction) {
 		const weight = interaction.options.getString('weight');
 		const exercise = interaction.options.getString('exercise');
 
 		await interaction.reply({
-			content: `You have submitted ${weight}kg for ${exercise}`,
+			content: `You have submitted ${weight}kg for ${exercise}.`,
 		});
 	},
 };
---
+++ utils/exerciseChoices.js
@@ -0,0 +1,5 @@
+module.exports = [
+	{ name: 'Bench Press', value: 'Bench Press' },
+	{ name: 'Squat', value: 'Squat' },
+	{ name: 'Deadlift', value: 'Deadlift' },
+];
