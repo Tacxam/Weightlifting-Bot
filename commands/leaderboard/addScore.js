a39c95fe4d0180f7cb279ac7f9ba870df6f6f23e
Liam Greer <Tacxam7@gmail.com>
2026-02-20 17:54:26 +1100

    added self contained addScore

--- commands/leaderboard/addScore.js
+++ commands/leaderboard/addScore.js
@@ -1,36 +1,121 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageFlags } = require('discord.js');
-const exerciseChoices = require('../../utils/exerciseChoices.js');
-const { setPending } = require('../../utils/pendingSubmission.js');
+const {
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  MessageFlags,
+  ComponentType,
+} = require("discord.js");
+const exerciseChoices = require("../../utils/exerciseChoices.js");
+const { setPending, getPending, deletePending } = require("../../utils/pendingSubmission.js");
+
+async function buttonHandler(interaction) {
+  // Remove buttons
+  await interaction.update({
+    components: [],
+  });
+
+  const pending = getPending(interaction.user.id);
+  confirmed = false;
+  // Confirm Button
+  if (interaction.customId === "confirm") {
+    // If there is no pending (expired?)
+    if (!pending) {
+      return interaction.update({
+        content:
+          "No pending submission found, your submission may have expired. Please try running the command again.",
+        components: [],
+      });
+    }
+
+    confirmed = true;
+
+    // ... functionality
+
+    deletePending(interaction.user.id);
+  }
+
+  // Cancel Button
+  if (interaction.customId === "cancel") {
+    deletePending(interaction.user.id);
+  }
+
+  // Handle text outputs
+  if (confirmed) {
+    await interaction.channel.send({
+      content: `${interaction.user} submitted **${pending.weight}kg** for **${pending.exercise}**`,
+    });
+  } else {
+    await interaction.followUp({
+      content: "Submission cancelled, nothing was submitted.",
+      flags: MessageFlags.Ephemeral,
+    });
+  }
+};
+
 
 // Add score to leaderboard (Delete any previous score from the leaderboard)
 module.exports = {
-	name: 'addscore',
-	data: new SlashCommandBuilder()
-		.setName('addscore')
-		.setDescription('Add a score to the leaderboard.')
-		.addStringOption((option) => option.setName('weight').setDescription('The weight being submitted.').setRequired(true))
-		.addStringOption((option) => option.setName('exercise').setDescription('The exercise being submitted.').setRequired(true).addChoices(...exerciseChoices)),
-	async execute(interaction) {
-		// Store values from options
-		const weight = interaction.options.getString('weight');
-		const exercise = interaction.options.getString('exercise');
+  name: "addscore",
+  data: new SlashCommandBuilder()
+    .setName("addscore")
+    .setDescription("Add a score to the leaderboard.")
+    .addStringOption((option) =>
+      option
+        .setName("weight")
+        .setDescription("The weight being submitted.")
+        .setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("exercise")
+        .setDescription("The exercise being submitted.")
+        .setRequired(true)
+        .addChoices(...exerciseChoices),
+    ),
+  async execute(interaction) {
+    // Store values from options
+    const weight = interaction.options.getString("weight");
+    const exercise = interaction.options.getString("exercise");
 
-		// Add entry to the pending object
-		setPending(interaction.user.id, { weight, exercise });
+    // Add entry to the pending object
+    setPending(interaction.user.id, { weight, exercise });
 
-		// Create button components
-		const row = new ActionRowBuilder().addComponents(
-			new ButtonBuilder()
-				.setCustomId('confirm').setLabel('Confirm').setStyle(ButtonStyle.Success),
-			new ButtonBuilder()
-				.setCustomId('cancel').setLabel('Cancel').setStyle(ButtonStyle.Danger),
-		);
+    // Create button components
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("confirm")
+        .setLabel("Confirm")
+        .setStyle(ButtonStyle.Success),
+      new ButtonBuilder()
+        .setCustomId("cancel")
+        .setLabel("Cancel")
+        .setStyle(ButtonStyle.Danger),
+    );
 
-		// Request confirmation of submission
-		const msg = await interaction.reply({
-			content: `You want to submit ${weight}kg for ${exercise}.\nIs this correct?`,
-			components: [row],
-			flags: MessageFlags.Ephemeral,
-		});
-	},
+    // Request confirmation of submission
+    const msg = await interaction.reply({
+      content: `You want to submit ${weight}kg for ${exercise}.\nIs this correct?`,
+      components: [row],
+      flags: MessageFlags.Ephemeral,
+      withResponse: true,
+    });
+
+    const collector = msg.resource?.message?.createMessageComponentCollector({
+      filter: (i) => i.user.id == interaction.user.id,
+      time: 30000,
+      componentType: ComponentType.Button,
+    });
+
+    // Button selected
+    collector?.on('collect', buttonHandler)
+
+    // Button expiry
+    collector?.on('end', (i) => {
+      interaction.editReply({
+        content: 'Submission expired, Please try running the command again.',
+        components: [],
+      })
+    })
+  },
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,35 +1,37 @@
-const { Events, Collection } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
+const { Events, Collection } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const handlers = new Collection();
 
 // Require handlers
-const handlersPath = path.join(__dirname, 'handlers');
-const handlerFiles = fs.readdirSync(handlersPath).filter((file) => file.endsWith('.js'));
+const handlersPath = path.join(__dirname, "handlers");
+const handlerFiles = fs
+  .readdirSync(handlersPath)
+  .filter((file) => file.endsWith(".js"));
 // For each file get their filePaths and put the handler file objects into a temporary variable 'handler' before putting them into the handlers Collection
 for (const file of handlerFiles) {
-	const filePath = path.join(handlersPath, file);
-	const handler = require(filePath);
+  const filePath = path.join(handlersPath, file);
+  const handler = require(filePath);
 
-	handlers.set(file.replace('.js', ''), handler);
+  handlers.set(file.replace(".js", ""), handler);
 }
 
 module.exports = {
-	name: Events.InteractionCreate,
-	async execute(interaction) {
-		// Handle Slash Commands
-		if (interaction.isChatInputCommand()) {
-			const handler = handlers.get('chatCommandHandler');
-			await handler(interaction);
-			return;
-		}
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    // Handle Slash Commands
+    if (interaction.isChatInputCommand()) {
+      const handler = handlers.get("chatCommandHandler");
+      await handler(interaction);
+      return;
+    }
 
-		// Handle Buttons # TODO make this only for addscore
-		else if (interaction.isButton()) {
-			console.log(interaction.commandName);
-			const handler = handlers.get('buttonHandler');
-			await handler(interaction);
-		};
-	},
+    // Handle Buttons # TODO make this only for addscore
+    // else if (interaction.isButton()) {
+    //   console.log(interaction.commandName);
+    //   const handler = handlers.get("buttonHandler");
+    //   await handler(interaction);
+    // }
+  },
 };
