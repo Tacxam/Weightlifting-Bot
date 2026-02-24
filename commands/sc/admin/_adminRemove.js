603f5376e89957ec3104cd90400b6c610824c872
Liam Greer <Tacxam7@gmail.com>
2026-02-24 21:53:45 +1100

    added buttonhandler placeholder to admin equivalent

--- commands/sc/admin/_adminRemove.js
+++ commands/sc/admin/_adminRemove.js
@@ -1,4 +1,68 @@
-const { SlashCommandSubcommandBuilder } = require("discord.js");
+const {
+  SlashCommandSubcommandBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  MessageFlags,
+  ComponentType,
+} = require("discord.js");
+const exerciseChoices = require("../../../utils/exerciseChoices.js");
+const {
+  setPending,
+  getPending,
+  deletePending,
+} = require("../../../utils/pendingSubmission.js");
+
+// Button handling
+async function buttonHandler(interaction) {
+  // Remove buttons
+  await interaction.update({
+    components: [],
+  });
+
+  const pending = getPending(interaction.user.id);
+  let confirmed = false;
+  // Confirm Button
+  if (interaction.customId === "confirm") {
+    // If there is no pending (expired?)
+    if (!pending) {
+      return interaction.update({
+        content:
+          "No pending removal found, the command may have expired. Please try running the command again.",
+        components: [],
+      });
+    }
+
+    confirmed = true;
+
+    /*
+     ... functionality
+     Check for score
+     if score exists
+     delete score, reply saying score deleted
+     else, reply saying no score found
+    */
+
+    deletePending(interaction.user.id);
+  }
+
+  // Cancel Button
+  if (interaction.customId === "cancel") {
+    deletePending(interaction.user.id);
+  }
+
+  // Handle text outputs
+  if (confirmed) {
+    await interaction.channel.send({
+      content: `${interaction.user} removed their score for **${pending.exercise}**`,
+    });
+  } else {
+    await interaction.followUp({
+      content: "Command cancelled, nothing was removed.",
+      flags: MessageFlags.Ephemeral,
+    });
+  }
+}
 
 // Delete score from leaderboard
 module.exports = {
@@ -19,6 +83,9 @@ module.exports = {
         .setRequired(true),
     ),
   async execute(interaction) {
-    // Placeholder
+    const user = interaction.options.addStringOption("user");
+    const exercise = interaction.options.addStringOption("exercise");
+
+
   },
 };
