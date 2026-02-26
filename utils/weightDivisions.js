e72cca005aecc0d131e922d91ce0a613bbacb5d3
Liam Greer <Tacxam7@gmail.com>
2026-02-26 16:49:31 +1100

    added weight divisions

--- utils/weightDivisions.js
+++ utils/weightDivisions.js
@@ -1,3 +1,4 @@
-module.exports = [
-	{}
-];
+const maleWeightDivisions = [59, 66, 74, 83, 93, 105, 120];
+const femaleWeightDivisions = [47, 52, 57, 63, 69, 76, 84];
+
+module.exports = {maleWeightDivisions, femaleWeightDivisions};
