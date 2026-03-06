a916565d55e3fa3a42240806f3fa4b50631c1c75
Liam Greer <Tacxam7@gmail.com>
2026-03-07 00:00:14 +1100

    more changes to updateLeaderboard function

--- utils/updateLeaderboard.js
+++ utils/updateLeaderboard.js
@@ -1,13 +1,28 @@
 async function updateLeaderboardMessage(client, redis, exercise) {
-	const channel = await redis.get(`lbchannel:${exercise}`);
-	const msg = await redis.get(`lbmsg:${exercise}`);
+  const channelId = await redis.get(`lbchannel:${exercise}`);
+  const msgId = await redis.get(`lbmsg:${exercise}`);
 
-	const top = await redis.zRevRangeWithScores(exercise, 0, 9);
+  const top = await redis.zRevRangeWithScores(exercise, 0, 9);
 
-	let content = `**${exercise} Leaderboard (Top 10):**`;
-	if (!top.length) {
-		content = "No content yet";
-	}
+  let content = `**${exercise} Leaderboard (Top 10):**`;
+  if (!top.length) {
+    content = "No content yet";
+  }
 
-	
+  // Map each array user and score to a string
+  content += top
+    .map(
+      (element, index) =>
+        `${index + 1}. <@${element.value} - **${element.score}**`,
+    )
+    .join("\n");
+
+  // Find channel
+  const channel = await client.channels.fetch(channelId);
+  if (!channel?.isTextBased()) return;
+
+  const msg = await channel.messages.fetch(msgId);
+  await msg.edit({ content });
 }
+
+module.exports = { updateLeaderboardMessage };
