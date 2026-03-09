3580d745e1ca743d2f1f9da3fe5eefa31f0dfff0
Liam Greer <Tacxam7@gmail.com>
2026-03-09 16:11:00 +1100

    skeleton code in lb/kg converter

--- commands/sc/lbKgConversion.js
+++ commands/sc/lbKgConversion.js
@@ -1,2 +1,25 @@
 const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  name: "lb/kg converter",
+  data: new SlashCommandBuilder()
+    .setName("lb/kg converter")
+    .setDescription("Convert from lb to kg or vice versa")
+    .addStringOption((option) =>
+      option
+        .setName("Convert to")
+        .setDescription("Convert to lb/kg")
+        .setRequired(true)
+        .addChoices(),
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("weight")
+        .setDescription("Weight being converted")
+        .setRequired(true),
+    ),
+
+		async execute(interaction) {
+			
+		}
+};
