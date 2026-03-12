568af654cfe40f0d7ee4b80ea320f945dbf32db4
Liam Greer <Tacxam7@gmail.com>
2026-03-13 00:05:44 +1100

    protein command implemented

--- commands/sc/optimalProtein.js
+++ commands/sc/optimalProtein.js
@@ -1,2 +1,27 @@
 const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  name: "protein",
+  data: new SlashCommandBuilder()
+    .setName("protein")
+    .setDescription(
+      "Calculate optimal protein intake for the inputted bodyweight",
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("bodyweight")
+        .setDescription("Bodyweight, not much else to say")
+        .setRequired(true),
+    ),
+
+  async execute(interaction) {
+    const bodyweight = interaction.options.getNumber("bodyweight");
+
+    const lowerBound = (1.6 * bodyweight).toFixed(0);
+		const upperBound = (2.2 * bodyweight).toFixed(0);
+
+    return interaction.reply({
+      content: `Ideal protein intake for a bodyweight of **${bodyweight}kg** is between **${lowerBound}g** to **${upperBound}g** (1.6 - 2.2 g/kg) per day during a bulk.\nHigher protein intake may be recommended on a cut.`,
+    });
+  },
+};
